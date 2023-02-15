c546c54631e15df15664c747958082218ffdc123
Helio Frota <00hf11@gmail.com>
2023-02-15 08:53:51 -0300

    feat: enabling otel-js

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,6 +1,10 @@
 import App from './App';
 import { createRoot } from 'react-dom/client';
 
+import webTracer from './webTracer';
+
+webTracer();
+
 const container = document.getElementById('app');
 const root = createRoot(container!);
 root.render(<App />);
